528f4c25efc6bef3b29e59202b74ca5db51ba4d8
nonlinearthink <2832203391@qq.com>
2023-10-23 17:29:01 +0800

    vector3 test

--- tests/vector3_test.rs
+++ tests/vector3_test.rs
@@ -33,3 +33,77 @@ fn vector_cross_test() {
     // the result is correct
     assert_vector3_eq!(result, 6., -5., 1.);
 }
+
+#[test]
+fn vector_length_squared_test() {
+    let v = Vector3::new(1., 2., 2.);
+    assert_eq!(v.length_squared(), 9.);
+}
+
+#[test]
+fn vector_length_test() {
+    let v = Vector3::new(1., 2., 2.);
+    assert_eq!(v.length(), 3.);
+}
+
+#[test]
+fn vector_normolize_test() {
+    let v = Vector3::new(1., 1., f32::sqrt(2.));
+    assert_vector3_eq!(v.normolize(), 1. / 2., 1. / 2., f32::sqrt(2.) / 2.);
+}
+
+#[test]
+fn vector_index_test() {
+    let v = Vector3::new(1., 2., 3.);
+    assert_eq!(v[0], 1.);
+    assert_eq!(v[1], 2.);
+    assert_eq!(v[2], 3.);
+}
+
+#[test]
+fn vector_add_test() {
+    let v1 = Vector3::new(1., 2., 4.);
+    let v2 = Vector3::new(1., 3., 9.);
+    let result = &v1 + &v2;
+    // the origin vectors not change
+    assert_vector3_eq!(v1, 1., 2., 4.);
+    assert_vector3_eq!(v2, 1., 3., 9.);
+    // the result is correct
+    assert_vector3_eq!(result, 2., 5., 13.);
+}
+
+#[test]
+fn vector_sub_test() {
+    let v1 = Vector3::new(1., 2., 4.);
+    let v2 = Vector3::new(1., 3., 9.);
+    let result = &v2 - &v1;
+    // the origin vectors not change
+    assert_vector3_eq!(v1, 1., 2., 4.);
+    assert_vector3_eq!(v2, 1., 3., 9.);
+    // the result is correct
+    assert_vector3_eq!(result, 0., 1., 5.);
+}
+
+#[test]
+fn vector_mul_test() {
+    let v1 = Vector3::new(1., 2., 4.);
+    let v2 = Vector3::new(1., 3., 9.);
+    let result = &v1 * &v2;
+    // the origin vectors not change
+    assert_vector3_eq!(v1, 1., 2., 4.);
+    assert_vector3_eq!(v2, 1., 3., 9.);
+    // the result is correct
+    assert_vector3_eq!(result, 1., 6., 36.);
+}
+
+#[test]
+fn vector_div_test() {
+    let v1 = Vector3::new(1., 2., 4.);
+    let v2 = Vector3::new(1., 3., 9.);
+    let result = &v2 / &v1;
+    // the origin vectors not change
+    assert_vector3_eq!(v1, 1., 2., 4.);
+    assert_vector3_eq!(v2, 1., 3., 9.);
+    // the result is correct
+    assert_vector3_eq!(result, 1., 1.5, 2.25);
+}
